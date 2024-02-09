f9fe8df22ff4f1fd853c2d9a90a5a3de97057aee
bradleymbuvi <bradwent@gmail.com>
2024-02-09 17:06:37 +0300

    Updated Components

---
+++ my-react-app/src/Components/App.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+function App() {
+  const [transaction, setTransaction] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:8001/transactions")
+     .then((resp)=> resp.json())
+     .then(data => setTransaction(data.transaction))
+  })
+
+
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+    </div>
+  );
+}
+export default App;
